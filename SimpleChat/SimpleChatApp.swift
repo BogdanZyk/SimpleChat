47f6c4e4cea15b992a514c7273d2cd6088ecf4fe
Bogdan <zykovbogdan07@gmail.com>
2022-12-16 13:44:26 +0300

    Initial Commit

---
+++ SimpleChat/SimpleChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChatApp.swift
+//  SimpleChat
+//
+//  Created by Богдан Зыков on 16.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
